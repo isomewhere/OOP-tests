fa2bde85a9a820077e5f2682e2939664a32401e9
isomewhere <isomewhere@mail.ru>
2024-07-16 14:25:52 +0300

    OOP

--- phone/src/main/java/readers/Reader.java
+++ phone/src/main/java/readers/Reader.java
@@ -16,23 +16,25 @@ public class Reader {
         System.out.println(fio + "Взял " + numBooks);
     }
 
-    public void takeBooks(String...booksTitle ) {
+    public void takeBooks(String... booksTitle) {
         System.out.println(fio + "Взял книги" + Arrays.toString(booksTitle));
     }
 
-    public void returnBook (String...booksTitle) {
+    public void returnBook(String... booksTitle) {
         System.out.println(fio + "Вернул Книги " + Arrays.toString(booksTitle));
 
     }
-    public void takeBooks(Book...books) {
-        System.out.println(fio + "Взфл Книги " );
+
+    public void returnBook(int numsOfBooks) {
+        System.out.println(fio + "Вернул " + numsOfBooks);
+    }
+
+    public void takeBooks(Book... books) {
+        System.out.println(fio + "Взял Книги ");
         for (int i = 0; i < books.length; i++) {
             System.out.println(books[i]);
 
-        } {
-
         }
-
     }
 
     public Reader(int dateOfBirth, String faculty, String fio, String phoneNumber, int readersNum) {
@@ -44,33 +46,39 @@ public class Reader {
     }
 
 
-     class Book {
-private String title;
-private String author;
+    static class Book {
+        private final String title;
+        private final String author;
 
-         public Book(String author, String title) {
-             this.author = author;
-             this.title = title;
-         }
-         @Override
-         public String toString() {
-             return title + author;
-         }
-     }
+        public Book(String author, String title) {
+            this.author = author;
+            this.title = title;
+        }
 
-    public void main(String[] args) {
-        Reader Igor = new Reader(1985,"Eng", "Петров В.В", "89654", 7);
-
-        Book book1 = new Book("Автор1", "Приключения");
-        Book book2 = new Book("Автор2", "Словарь");
-        Book book3 = new Book("Автор3", "Энциклопедия");
-
-        Igor.takeBook(3);
-        Igor.takeBooks("Приключения", "Словарь", "Энциклопедия");
-        Igor.takeBooks(book1,book2,book3);
-
-        Igor.returnBook("Приключения", "Словарь", "Энциклопедия");
+        @Override
+        public String toString() {
+            return title + author;
+        }
     }
+
+    public static class Main {
+
+        public static void main(String[] args) {
+            Reader Igor = new Reader(1985, "Eng", "Петров В.В", "89654", 7);
+
+            Book book1 = new Book("Автор1", "Приключения");
+            Book book2 = new Book("Автор2", "Словарь");
+            Book book3 = new Book("Автор3", "Энциклопедия");
+
+            Igor.takeBook(3);
+            Igor.takeBooks("Приключения", "Словарь", "Энциклопедия");
+            Igor.takeBooks(book1, book2, book3);
+
+            Igor.returnBook("Приключения", "Словарь", "Энциклопедия");
+            Igor.returnBook(3);
+        }
+    }
+
 }
 
 
